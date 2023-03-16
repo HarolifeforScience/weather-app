31280650caab8eda01a588615b229492593f365f
HarolifeforScience <haroldonell21@gmail.com>
2023-03-16 07:21:37 +0100

    changed settings#

--- weather_ui.py
+++ weather_ui.py
@@ -1,1 +1,20 @@
-import PySide6 
+from PySide6 import *
+from multiprocessing.pool import ThreadPool
+import requests 
+import plyer
+
+
+class MainWindow():
+    pass
+
+def __init__(self):
+    pass
+
+def getData(self):
+    pass
+
+def getActData(self):
+    pass
+
+def notify(self):
+    pass
